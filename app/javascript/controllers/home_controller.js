8b4e9e618d0afdd4f75bb5a07f250bdbcd957f39
mohdmohiuddin <moinmohammed22@gmail.com>
2021-06-18 10:39:51 +0530

    removed dom references in stimulus

--- app/javascript/controllers/home_controller.js
+++ app/javascript/controllers/home_controller.js
@@ -1,20 +1,19 @@
 import {  Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["heading", "total", "transactions"]
-  static values = {price: String}
+  static targets = ["heading", "total", "transactions", "quantity"]
+  static values = {price: Number}
   connect() {
-    console.log(this.priceValue)
   }
 
   calculatePrice() {
-    const quantity = document.getElementById("price-drop-down").value
-    const price = document.getElementById("price").value
+    const quantity = this.quantityTarget.value
+    const price = this.priceValue
     this.totalTarget.innerHTML = (quantity * price)
   }
   updateCart() {
-    const quantity = document.getElementById("price-drop-down").value
-    const price = document.getElementById("price").value
+    const quantity = this.quantityTarget.value
+    const price = this.priceValue
     this.transactionsTarget.insertAdjacentHTML('beforeEnd', '<div class="m-1">'+quantity+' x '+price+' = '+(quantity*price)+'</div>');
   }
 }
